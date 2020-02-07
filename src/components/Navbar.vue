fa318cc67dbdfd05f135f12ac2d4b20fc45140ac
Sam Green <sam.bg@hotmail.com>
2020-02-07 21:16:41 +1300

    Added routes js file for defining paths to vue pages, created basic nav bar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,14 @@
 <template>
     <div>
-        
+        <nav class="navbar navbar-expand-lg navbar-light">
 
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="navbar-nav mr-auto navbar-center">
+                    <router-link class="nav-item nav-link" to="/">Home</router-link>
+                </div>
+            </div>
 
+        </nav>
     </div>
 </template>
 
---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import HomePage from './pages/Home.vue';
+
+const routes = [
+    {
+        path: '/',
+        component: HomePage
+    }
+];
+
+export default routes;
