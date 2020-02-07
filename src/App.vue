95b665b011815bc95353e0ba4395ed38eed2c21e
Sam Green <sam.bg@hotmail.com>
2020-02-07 21:52:42 +1300

    Added navbar to the app, added a logo home button

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
-    
+    <Navbar></Navbar>
     
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar';
 
 export default {
   name: 'app',
   components: {
-    
+    'Navbar': Navbar
   }
 }
 </script>
@@ -22,6 +23,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,6 +2,14 @@
     <div>
         <nav class="navbar navbar-expand-lg navbar-light">
 
+            <a class="navbar-brand" href="/">
+                <img src="../assets/logo.png">
+                Klicken Creative
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
             <div class="collapse navbar-collapse" id="navbarNav">
                 <div class="navbar-nav mr-auto navbar-center">
                     <router-link class="nav-item nav-link" to="/">Home</router-link>
@@ -13,5 +21,8 @@
 </template>
 
 <style>
-
+    .navbar {
+    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    font-size: 200%;
+    }
 </style>
