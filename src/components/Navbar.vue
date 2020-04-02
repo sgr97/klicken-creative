58fe9539afaba85f34fde9125aae61fe4842efad
Sam Green <sam.bg@hotmail.com>
2020-04-03 11:49:32 +1300

    Removed title from logo, resized logo to 600x600

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,25 +3,22 @@
         <nav class="navbar navbar-expand-lg navbar-light flex-column">
 
             <a class="navbar-brand" href="/">
-                <img src="../assets/company-logo_60x60.png">
-                Klicken Creative
+                <img src="../assets/company-logo_600x600.png">
             </a>
 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
-            <div class="flex-row">
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <div class="navbar-nav mr-auto navbar-center">
-                        <router-link class="nav-item nav-link" to="/">Home</router-link>
-                    </div>
+            
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="navbar-nav mr-auto navbar-center">
+                    <router-link class="nav-item nav-link" to="/">Home</router-link>
                 </div>
+            </div>
 
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <div class="navbar-nav mr-auto navbar-center">
-                        <router-link class="nav-item nav-link" to="/">Gallery</router-link>
-                    </div>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="navbar-nav mr-auto navbar-center">
+                    <router-link class="nav-item nav-link" to="/">Gallery</router-link>
                 </div>
             </div>
 
