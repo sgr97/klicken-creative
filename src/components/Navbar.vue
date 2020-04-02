940e93a37492801e3b6b18ac8d3b2f352d867aa1
Sam Green <sam.bg@hotmail.com>
2020-04-03 11:47:28 +1300

    Added a gallery router link to the nav bar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,27 @@
 <template>
     <div>
-        <nav class="navbar navbar-expand-lg navbar-light">
+        <nav class="navbar navbar-expand-lg navbar-light flex-column">
 
             <a class="navbar-brand" href="/">
                 <img src="../assets/company-logo_60x60.png">
                 Klicken Creative
             </a>
+
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <div class="navbar-nav mr-auto navbar-center">
-                    <router-link class="nav-item nav-link" to="/">Home</router-link>
+            <div class="flex-row">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <div class="navbar-nav mr-auto navbar-center">
+                        <router-link class="nav-item nav-link" to="/">Home</router-link>
+                    </div>
+                </div>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <div class="navbar-nav mr-auto navbar-center">
+                        <router-link class="nav-item nav-link" to="/">Gallery</router-link>
+                    </div>
                 </div>
             </div>
 
@@ -24,7 +33,7 @@
 
 <style>
     .navbar {
-    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-    font-size: 200%;
+        font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        font-size: 200%;
     }
 </style>
